1bc79ae4c2fb9650c21465cd8a6895e71026132c
GabrielSantos1997 <84352562+GabrielSantos1997@users.noreply.github.com>
2022-10-09 10:09:27 -0300

    Add files via upload

---
+++ src/main/java/saojudas/firstexercise/FirstExercise.java
@@ -0,0 +1,13 @@
+package saojudas.firstexercise;
+
+/**
+ *
+ * @author Gabriel Jerônimo dos Santos
+ * RA: 818125147
+ *
+ */
+public class FirstExercise {
+    public static void main(String[] args) {
+        System.out.println("Alo mundo!");
+    }
+}
